040da46346ecb57b3551814a6bfbc3ed7bf58c1c
soahg <mislamsohag@gmail.com>
2022-05-04 13:17:39 +0600

    db connected

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-//use middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECURET_KEY}@cluster0.qgvx9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("database Connect")
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-});
+})
 
 app.listen(port, () => {
-    console.log('Example app listening on port', port)
+    console.log(`Example app listening on port ${port}`)
 })
